af88897eccd551c26f59da88b2fbc49468aaf24c
Martin Vaughn <martinvaughn.io@gmail.com>
2023-07-13 15:42:33 -0700

    palindrome checker

--- reverse-string.js
+++ reverse-string.js
@@ -20,4 +20,11 @@ function reverse_better(str) {
   return reverse;
 }
 
+function check_palindrome(str) {
+  if (reverse_better(str) === str) { return true}
+  else return false;
+}
+
 console.log("Reversed: ", reverse_better("hsiloof yats .yrgnuh yats"));
+console.log("Is scoop a palindrome? ", check_palindrome("scoop"));
+console.log("Is racecar a palindrome? ", check_palindrome("racecar"));
